1842b2c9eef079ea14169c771fd48b72e9d36cc9
lia-na-2210 <naneko.bl@gmail.com>
2022-11-11 16:48:21 -0600

    Everything at once because I forgot to do it one by one

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let string = ''
+    if (num < 0) {
+        return 'ERROR'
+    } else {
+      for (i = 0; i < num; i++) {
+        string += word
+        }  
+    }
+    return string
 };
+
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+    let myText = phrase;
+    const myArray = myText.split("");
 
-};
+    const reversedArray = myArray.reverse();
+    return reversedArray.join('');
+}; 
 
 // Do not edit below this line
 module.exports = reverseString;
