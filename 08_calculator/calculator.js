504c59ae548f06adbe6b8ea9a3c878e78c5c1aa0
lia-na-2210 <naneko.bl@gmail.com>
2022-11-11 20:10:51 -0600

    how come there is no easy way to calculate a factorial!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,54 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let result = 0;
+  result = num1 + num2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let result = 0;
+  result = num1 - num2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(numbs) {
+	let result = 0;
+  numbs.forEach(sumThem);
+  
+  function sumThem(item) {
+    result += item;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numbs) {
+  let result = 1;
+  numbs.forEach(multiplyThem);
+  
+  function multiplyThem(item) {
+    result = result * item;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let result = 0
+  result = num1 ** num2
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+  if (num == 0 || num == 1) {
+    return 1;
+  }
+
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
 };
+
 
 // Do not edit below this line
 module.exports = {
