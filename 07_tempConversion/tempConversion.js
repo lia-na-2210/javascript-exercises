5a85ed150fa01b078656dc14b0dc52e1bc9323d8
lia-na-2210 <naneko.bl@gmail.com>
2022-11-11 18:59:54 -0600

    learned how to turn a string back into a number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,24 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newTemp = (temp -32) * (5/9);
+  if (newTemp == 0) {
+    return newTemp;
+  } else {
+    newTemp = Number(newTemp.toFixed(1));
+    return newTemp
+  }
+  
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let newTemp = (temp * (9/5)) + 32;
+  if (newTemp == 0) {
+    return newTemp;
+  } else {
+    newTemp = Number(newTemp.toFixed(1));
+    return newTemp;
+  }
 };
+
 
 // Do not edit below this line
 module.exports = {
