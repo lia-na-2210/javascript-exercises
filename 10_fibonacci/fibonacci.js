d4304c1d0cd189a05b0255c8a344d4db2056514b
lia-na-2210 <naneko.bl@gmail.com>
2023-01-08 20:23:57 -0600

    for loop, no need for array

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(place) {
+    const number = parseInt(place);
+    if (number < 0) {
+        //console.log('oops');
+        return 'OOPS';
+    } else if (number == 0) {
+        //console.log(0);
+        return 0;
+    }
+
+    let x = 0; 
+    let y = 1; 
+
+    for (let i = 1; i < number; i++) {
+        const previous = y;
+        
+        y = x + y; 
+        x = previous; 
+        
+    }
+
+    return y;
+    //console.log(y)
 
 };
+
+fibonacci(-25)
 
 // Do not edit below this line
 module.exports = fibonacci;
