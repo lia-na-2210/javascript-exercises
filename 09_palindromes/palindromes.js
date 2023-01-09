2fd6fc03ff73b229b27f43198337d4cbab2ac14f
lia-na-2210 <naneko.bl@gmail.com>
2023-01-08 18:39:05 -0600

    used array.filter

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
+const array = Array.from(word.split(' ').join('').toLowerCase())
 
+const filtered = array.filter(letter => (letter !== ',' && letter !== '!' && letter !== '.'));
+const string = filtered.join('');
+
+const newString = filtered.reverse().join('');
+if (string == newString) {
+   return true
+} else {
+    return false
+}
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
