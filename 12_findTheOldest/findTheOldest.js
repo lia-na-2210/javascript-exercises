e3e0fd97d0dfabcf279014936af133fdc810b978
lia-na-2210 <naneko.bl@gmail.com>
2023-01-09 20:50:55 -0600

    gotta study arrays more

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const currentAge = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })
+};
+
+const getAge = function(death, birth) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth
 };
 
 // Do not edit below this line
